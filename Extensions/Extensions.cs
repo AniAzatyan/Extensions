8f8872aac12b8599d5dce4592cc003ef35ed1768
Ani Azatyan <aniazatyan.2001@gmail.com>
2021-10-13 19:50:41 +0400

    Add project files.

---
+++ Extensions/Extensions.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+    public static class Extensions
+    {
+        public static bool GetOddIndexNumbers(this IEnumerable<int> source, out List<int> lll)
+        {
+            lll = new List<int>();
+            bool hasElement = false;
+            int count = 0;
+            foreach (var item in source)
+            {
+                if (count % 2 == 1)
+                {
+                    lll.Add(item);
+                }
+                count += 1;
+                hasElement = true;
+            }
+            return hasElement;
+        }
+        public static bool GetSumOfEvenNumbers(this IEnumerable<int> source, out int sum)
+        {
+            bool hasElement = false;
+            sum = 0;
+            foreach (var item in source)
+            {
+                if (item % 2 == 0)
+                {
+                    sum += item;
+                    hasElement = true;
+                }
+            }
+            return hasElement;
+        }
+        public static bool _IsNullOrEmpty<T>(this IEnumerable<T> source)
+        {
+            return source != null && source.Count() > 0;
+        }
+        public static bool WordFromLettersOfString(this string source, out string word)
+        {
+            bool hasElement = false;
+            word = default(string);
+            int count = 0;
+            foreach (var item in source)
+            {
+                if (count % 3 == 0)
+                {
+                    word += item;
+                }
+                count += 1;
+                hasElement = true;
+            }
+            return hasElement;
+        }
+        public static string FirstAndLast(this string source)
+        {
+            return (String.Concat( source[0] ,source[source.Length - 1]));
+        }
+       
+    }
+}
---
+++ Extensions/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Extensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*----------------------------------------------------*/
+
+            string a = "abcdefghijklmnop";
+            bool b = a.WordFromLettersOfString(out string word);
+            Console.WriteLine(word);
+           
+            Console.WriteLine(a.FirstAndLast());
+           
+            /*----------------------------------------------------*/
+
+            List<int> numbers = new List<int>() { 12, 4, 5, 17, 2 };
+
+            numbers.GetSumOfEvenNumbers(out int sum);
+            Console.WriteLine(sum);
+
+            numbers.GetOddIndexNumbers(out List<int> lll);
+            foreach (var item in lll)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(numbers._IsNullOrEmpty());
+
+        }
+    }
+}
